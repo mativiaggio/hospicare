43cb8b0c4e557caf55eaa87d40bf6822dcb7d3b4
Matias Viaggio <matiasviaggio59@gmail.com>
2024-12-03 15:51:51 -0300

    Status Error Fixing - v0.1.0

--- src/app/(protected)/soporte/page.tsx
+++ src/app/(protected)/soporte/page.tsx
@@ -1,13 +1,5 @@
-import { Metadata } from "next";
 import TicketsDataContainer from "@/features/ticketing-system/components/tickets-data-container";
 
-export const metadata: Metadata = {
-  title: "Ticketing System Dashboard",
-  description: "View and manage your support tickets",
-};
-
 export default function DashboardPage() {
-  return (
-      <TicketsDataContainer />
-  );
+  return <TicketsDataContainer />;
 }
