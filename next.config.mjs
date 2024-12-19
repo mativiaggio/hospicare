47c2095f9d3f644509b163a573d0118041fb067d
Matias Viaggio <matiasviaggio59@gmail.com>
2024-12-19 12:18:58 -0300

    Dynamic Server Usage Error - v0.1.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      appDir: true, // Si estás usando la carpeta "app/"
-    },
-    output: "standalone", // Para despliegues más fáciles en plataformas como Vercel
-  };
-  
+const nextConfig = {};
+
 export default nextConfig;
